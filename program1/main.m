dccdaa267ef18c137935662dd596e0ea5284487e
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2021-08-26 15:39:20 +0700

    Initial Commit

---
+++ program1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  program1
+//
+//  Created by DevOpsThinh on 8/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
